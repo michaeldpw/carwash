85b1e34a1fea6bd94494f144e2f069e19e1ae36f
peiwendu <peiwendu@peiwendus-MacBook-Pro.local>
2019-10-20 22:25:52 +1100

    server

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ var app = express();
 const port = process.env.PORT || 9000 ;
 
 if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); 
+    // });
+}
+
+
+if(process.env.NODE_ENV === 'production'){
     app.use(express.static( 'client/build' ));
 
     // app.get('*', (req, res) => {
