ee3cacb4f8428d1994ebcf7dc032b02b48f7a343
peiwendu <peiwendu@peiwendus-MacBook-Pro.local>
2019-10-20 22:28:57 +1100

    server

---
+++ client/build/precache-manifest.f46c82228ac39a06b016c9e91a65b61d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ec4542b14149ae6e9285cd88de6d07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "441f715cc871f3e2b987",
+    "url": "/static/css/2.970bfeb6.chunk.css"
+  },
+  {
+    "revision": "70d2360078e6aa38f565",
+    "url": "/static/css/main.50233511.chunk.css"
+  },
+  {
+    "revision": "441f715cc871f3e2b987",
+    "url": "/static/js/2.9eb99376.chunk.js"
+  },
+  {
+    "revision": "70d2360078e6aa38f565",
+    "url": "/static/js/main.a7d67906.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "b2cb6fab5fa6a42eddd74f8b0fa06011",
+    "url": "/static/media/background.b2cb6fab.jpg"
+  }
+]);
