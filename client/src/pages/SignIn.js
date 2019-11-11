38682ab95243776308a98b7ddd658d49a89d294d
peiwendu <peiwend@student.unimelb.edu.au>
2019-11-11 11:22:56 +1100

    p

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -2,11 +2,37 @@ import React from 'react'
 import Particles from 'react-particles-js';
 import './pages.css'
 import SignInPanel from '../components/SignInPanel'
-const particlesOptions = {
-    particles: {
-        "particles": {
+// const particlesOptions = {
+//     particles: {
+//         "particles": {
+// 	        "number": {
+// 	            "value": 80
+// 	        },
+// 	        "size": {
+// 	            "value": 3
+// 	        }
+// 	    },
+// 	    "interactivity": {
+// 	        "events": {
+// 	            "onhover": {
+// 	                "enable": true,
+// 	                "mode": "repulse"
+// 	            }
+// 	        }
+// 	    }
+     
+//     }
+//   };
+class SignIn extends React.Component{
+    render(){
+        return (
+            <div className="signin">
+				<SignInPanel />
+                <Particles style={{position: 'absolute', top: '0', left: '0'}}
+    params={{
+	    "particles": {
 	        "number": {
-	            "value": 80
+	            "value": 50
 	        },
 	        "size": {
 	            "value": 3
@@ -20,15 +46,8 @@ const particlesOptions = {
 	            }
 	        }
 	    }
-     
-    }
-  };
-class SignIn extends React.Component{
-    render(){
-        return (
-            <div className="signin">
-                <Particles params={particlesOptions} width="100%" height="100vh"/> 
-                <SignInPanel />
+	}} />
+                
             </div>
             
                    
